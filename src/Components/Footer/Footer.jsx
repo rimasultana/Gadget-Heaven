f0abe32eb7bc91bf461c126434e3cf7602a6ec08
rimasultana <your-email@example.com>
2024-11-03 22:55:13 +0600

    footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,30 @@
+export default function Footer() {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold">Gadget Heaven</h1>
+        <p>Leading the way in cutting-edge technology and innovation.</p>
+      <footer className="footer bg-neutral text-neutral-content p-10">
+        <nav>
+          <h6 className="footer-title">Services</h6>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Company</h6>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Legal</h6>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </nav>
+      </footer>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,37 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Statistics from './Components/Statistics/Statistics.jsx';
+import Home from './Components/Home/Home.jsx';
+import Dashboard from './Components/Dashboard/Dashboard.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"/statistics",
+        element:<Statistics/>
+      },
+      {
+        path:"/dashboard",
+        element:<Dashboard/>
+      }
+    ]
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </StrictMode>,
 )
