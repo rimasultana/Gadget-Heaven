0f8f42fea8f893f379cfb7b193bf89f04fbfa679
rimasultana <rimasultana20225@gmail.com>
2025-01-11 20:45:53 +0600

    firebase added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAVY0oooS2QVg5PTFRNmj6250kPvN7bqTs",
+  authDomain: "gadget-heaven-a5ac4.firebaseapp.com",
+  projectId: "gadget-heaven-a5ac4",
+  storageBucket: "gadget-heaven-a5ac4.firebasestorage.app",
+  messagingSenderId: "274520988082",
+  appId: "1:274520988082:web:08606c77e71c4190b86cab"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
